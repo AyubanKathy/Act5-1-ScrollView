f3221362c8adc3fe159c7462872e47426431175e
AyubanKathy <kathquileopas@gmail.com>
2023-11-17 17:29:49 +0800

    create

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView } from 'react-native-web';
+import { users } from './src/users';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ScrollView>
+        <Text style={styles.names}>LIST OF GUESTS</Text>
+        {users.map((user) => {
+          return (
+            <View>
+              <Text style={styles.names}>{user.name.lastname}, {user.name.firstname}{user.name.middlename ? `, ${user.name.middlename}.` : ''}</Text>
+              <Text style={styles.town_province}>{user.town}, {user.province}</Text>
+            </View>
+          )
+        })}
+      </ScrollView>
+      <StatusBar style="auto"/>
     </View>
   );
 }
@@ -16,5 +28,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 220,
   },
+  names: {
+    flexDirection:'row',
+    fontFamily: 'Tahoma',
+    fontSize: 15,
+    fontWeight: 'bold',
+    width: 340,
+    marginTop: '18px',
+  },
+  town_province: {
+    fontFamily: 'Tahoma',
+    fontSize: 13,
+  }
 });
